f3cc2501001f8494d79237828c35ce73abd9ef97
Steven Sklar <steve@predata.com>
2019-12-14 10:00:05 -0500

    Remove capslock key I forgot about

--- keymap.c
+++ keymap.c
@@ -44,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // Base layer
   [BASE] = LAYOUT_ergodox(
       // left hand
-      KC_ESC,            KC_1,           KC_2,       KC_3,        KC_4,       KC_5,  KC_CAPSLOCK,
+      KC_ESC,            KC_1,           KC_2,       KC_3,        KC_4,       KC_5,  ____,
       KC_TAB,            KC_Q,           KC_W,       KC_E,        KC_R,       KC_T,  ____,
       LT(2,KC_ESC),      KC_A,           KC_S,       KC_D,        KC_F,       KC_G,
       OSM(MOD_LSFT),     KC_Z,           KC_X,       KC_C,        KC_V,       KC_B,  KC_BSLS,
